c4644fbb7a9a166ba2aa1394bae00f7d16effa3b
Daniel Hernandez Toledo <daniel.aht1993@gmail.com>
2025-02-13 18:13:55 -0300

    useReducer con TS

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,13 @@
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, FormEvent, Dispatch } from 'react'
 import { Activity } from '../types'
 import { categories } from '../data/categories'
+import { ActivityActions } from '../reducers/activity-reducer'
 
-export default function Form() {
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>
+}
+
+export default function Form({dispatch}: FormProps) {
 
   const [activity, setActivity] = useState<Activity>({
     category: 1,
@@ -12,17 +17,31 @@ export default function Form() {
 
   const handleOnChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
 
-    
+    const isNumberField = ['category', 'calories'].includes(e.target.id)
+
+    console.log(isNumberField)
 
     setActivity({
       ...activity,
-      [e.target.id]: e.target.value
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value
     })
+  }
+
+  const isValidActivity = () => {
+    const { name, calories } = activity
+    return name.trim() !== '' && calories > 0
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    dispatch({ type: 'save-activity', payload: { newActivity: activity } })
   }
 
   return (
     <form
       className='space-y-5 bg-white p-10 rounded-lg shadow-md'
+      onSubmit={handleSubmit}
     >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className='font-bold'>Categoria:</label>
@@ -70,8 +89,9 @@ export default function Form() {
 
       <input
         type="submit"
-        className='bg-gray-800 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold cursor-pointer rounded'
-        value='Guardar comida o ejercicio'
+        className='bg-gray-800 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold cursor-pointer rounded disabled:opacity-10'
+        value={activity.category === 1 ? 'Guardar comida' : 'Guardar ejercicio'}
+        disabled={!isValidActivity()}
       />
 
     </form>
